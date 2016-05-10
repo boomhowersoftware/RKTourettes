74ab9aa91ad8490b4602000b4b58d1fa8f4f417e
Glenn Rosen <glennrosen@mac.com>
2016-05-10 12:17:53 -0500

    Consent dummied up

---
+++ RKTourettes/ConsentDocument.swift
@@ -0,0 +1,40 @@
+//
+//  ConsentDocument.swift
+//  RKTourettes
+//
+//  Created by Glenn Rosen on 5/10/16.
+//  Copyright © 2016 Boomhower Software, LLC. All rights reserved.
+//
+
+import Foundation
+import ResearchKit
+
+public var ConsentDocument: ORKConsentDocument {
+    
+    let consentDocument = ORKConsentDocument()
+    consentDocument.title = "Sample Consent Document"
+    
+    let consentSectionTypes: [ORKConsentSectionType] = [
+        .Overview,
+        .DataGathering,
+        .Privacy,
+        .DataUse,
+        .TimeCommitment,
+        .StudySurvey,
+        .StudyTasks,
+        .Withdrawing
+    ]
+    
+    var consentSections: [ORKConsentSection] = consentSectionTypes.map { contentSectionType in
+        let consentSection = ORKConsentSection(type: contentSectionType)
+        consentSection.summary = "This would be a summary of what this consent section is geared to do"
+        consentSection.content = "This is the content of this section"
+        return consentSection
+    }
+    
+    consentDocument.sections = consentSections
+    
+    consentDocument.addSignature(ORKConsentSignature(forPersonWithTitle: nil, dateFormatString: nil, identifier: "ConsentDocumentParticipantSignature"))
+    
+    return consentDocument
+}
---
+++ RKTourettes/ConsentTask.swift
@@ -0,0 +1,32 @@
+//
+//  ConsentTask.swift
+//  RKTourettes
+//
+//  Created by Glenn Rosen on 5/10/16.
+//  Copyright © 2016 Boomhower Software, LLC. All rights reserved.
+//
+
+import Foundation
+import ResearchKit
+
+public var ConsentTask: ORKOrderedTask {
+    
+    var steps = [ORKStep]()
+    
+    var consentDocument = ConsentDocument
+    let visualConsentStep = ORKVisualConsentStep(identifier: "VisualConsentStep", document: consentDocument)
+    steps += [visualConsentStep]
+    
+    
+    let signature = consentDocument.signatures!.first! as ORKConsentSignature
+    
+    let reviewConsentStep = ORKConsentReviewStep(identifier: "ConsentReviewStep", signature: signature, inDocument: consentDocument)
+    
+    reviewConsentStep.text = "Review Consent!"
+    reviewConsentStep.reasonForConsent = "Consent to join study"
+    
+    steps += [reviewConsentStep]
+    
+    return ORKOrderedTask(identifier: "ConsentTask", steps: steps)
+}
+
